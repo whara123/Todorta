7d5f09c43b14130abce1285dc6fd8498d79299c4
whara123 <64121533+whara123@users.noreply.github.com>
2022-04-01 13:24:19 +0900

    ✨ Context생성 및 적용

--- src/index.js
+++ src/index.js
@@ -3,14 +3,17 @@ import ReactDOM from "react-dom";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import GlobalStyle from "./styles/GlobalStyle";
+import { TodoProvider } from "./TodoContext";
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <GlobalStyle />
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
+      <TodoProvider>
+        <GlobalStyle />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </TodoProvider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
