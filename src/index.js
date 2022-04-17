8236ff23a62597c5a0b1510dfbab65f5b911fb4f
whara123 <64121533+whara123@users.noreply.github.com>
2022-04-17 18:48:51 +0900

    :art: 랜드페이지에 three.js 적용

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import TodoCalender from "./pages/TodoCalender/TodoCalender";
+import TodoLand from "./pages/TodoLand/TodoLand";
 import GlobalStyle from "./styles/GlobalStyle";
 import { TodoProvider } from "./TodoContext";
 
@@ -17,6 +18,7 @@ root.render(
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/calender" element={<TodoCalender />} />
+          <Route path="/todoLand" element={<TodoLand />} />
         </Routes>
       </TodoProvider>
     </BrowserRouter>
--- src/pages/TodoLand/TodoLand.js
+++ src/pages/TodoLand/TodoLand.js
@@ -1,5 +1,19 @@
 import React from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
 
 export default function TodoLand() {
-  return <div>랜드</div>;
+  return (
+    <div>
+      <Canvas>
+        <OrbitControls />
+        <ambientLight intensity={1.5} />
+        <spotLight position={[10, 15, 10]} angle={0.3} />
+        <mesh position={[0, 0, 0]}>
+          <boxBufferGeometry attach="geometry" />
+          <meshLambertMaterial attach="material" color="orange" />
+        </mesh>
+      </Canvas>
+    </div>
+  );
 }
