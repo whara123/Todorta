639b29b1f49326a2bbe5e475c41d835409e187a0
whara123 <64121533+whara123@users.noreply.github.com>
2022-04-18 16:27:04 +0900

    🔥 3js 삭제

--- src/pages/TodoLand/TodoLand.js
+++ src/pages/TodoLand/TodoLand.js
@@ -1,19 +1,5 @@
 import React from "react";
-import { Canvas } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
 
 export default function TodoLand() {
-  return (
-    <div>
-      <Canvas>
-        <OrbitControls />
-        <ambientLight intensity={1.5} />
-        <spotLight position={[10, 15, 10]} angle={0.3} />
-        <mesh position={[0, 0, 0]}>
-          <boxBufferGeometry attach="geometry" />
-          <meshLambertMaterial attach="material" color="orange" />
-        </mesh>
-      </Canvas>
-    </div>
-  );
+  return <div>준비중입니다.</div>;
 }
