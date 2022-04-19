12befbd097ecbd6b93665d5ec0a0c34bce6fd1b5
whara123 <64121533+whara123@users.noreply.github.com>
2022-04-19 19:12:21 +0900

    🎨 랜드 페이지에서 달력으로 이동 버튼 추가

--- src/pages/TodoLand/TodoLand.js
+++ src/pages/TodoLand/TodoLand.js
@@ -1,5 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { BsFillCalendar2WeekFill } from "react-icons/bs";
+import Point from "../../components/Point/Point";
 
 export default function TodoLand() {
-  return <div>준비중입니다.</div>;
+  const [todoPoint, setTodoPoint] = useState(0);
+  useEffect(() => {
+    const todoPointData = localStorage.getItem("todoPoint");
+    if (todoPointData) {
+      setTodoPoint(parseInt(todoPointData, 10));
+    }
+  }, []);
+
+  const navigation = useNavigate();
+
+  const handleMove = () => {
+    navigation("/calender");
+  };
+
+  return (
+    <>
+      <div>준비중입니다.</div>
+      <Point todoPoint={todoPoint} />
+      <button type="button" onClick={handleMove}>
+        <BsFillCalendar2WeekFill />
+      </button>
+    </>
+  );
 }
