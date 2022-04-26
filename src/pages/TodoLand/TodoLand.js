ce1fe5b3e284f6ef1de72ed911adecc411b17392
whara123 <64121533+whara123@users.noreply.github.com>
2022-04-26 11:40:17 +0900

    💄 맵 이미지 테스트

--- src/pages/TodoLand/TodoLand.js
+++ src/pages/TodoLand/TodoLand.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from "react";
+import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
 import { BsFillCalendar2WeekFill } from "react-icons/bs";
 import Point from "../../components/Point/Point";
+import TestMap from "../../asset/image/testmap.png";
 
 export default function TodoLand() {
   const [todoPoint, setTodoPoint] = useState(0);
@@ -19,12 +21,23 @@ export default function TodoLand() {
   };
 
   return (
-    <>
-      <div>준비중입니다.</div>
+    <LandWrap>
+      <MapIamage src={TestMap} alt="맵 이미지" />
+
       <Point todoPoint={todoPoint} />
       <button type="button" onClick={handleMove}>
         <BsFillCalendar2WeekFill />
       </button>
-    </>
+    </LandWrap>
   );
 }
+
+const MapIamage = styled.img`
+  width: 50%;
+  margin: 0 auto;
+`;
+
+const LandWrap = styled.div`
+  width: 100%;
+  height: 100%;
+`;
