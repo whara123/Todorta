88da01f4114b8c841e0a59d5919d514d6dd7e147
whara123 <64121533+whara123@users.noreply.github.com>
2022-05-14 15:38:58 +0900

    ✨ 로그인 UI 스타일

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,28 +1,54 @@
-import React from "react";
+import React, { useState, useRef } from "react";
 import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
 
 export default function Home() {
   const navigation = useNavigate();
+  const [loginModal, setLoginModal] = useState();
+  const Dimd = useRef();
 
-  const handleMove = () => {
-    navigation("/calender");
+  const handleLogin = () => {
+    setLoginModal(!loginModal);
+    // navigation("/calender");
   };
 
   return (
-    <div>
+    <HomeBody>
       <HomeHead>
         <h1>TODORTA</h1>
         <p>당신의 진짜 ToDoList</p>
       </HomeHead>
       <HomeMain>
-        <MoveButton type="button" onClick={handleMove}>
-          시작하기
+        <MoveButton type="button" onClick={handleLogin}>
+          로그인
         </MoveButton>
+        {loginModal && (
+          <>
+            <LoginModal>
+              <LoginText>로그인 또는 회원가입</LoginText>
+              <LoginInputWrap>
+                <IdPassWardInput type="text" placeholder="아이디" />
+                <IdPassWardInput type="password" placeholder="비밀번호" />
+              </LoginInputWrap>
+              <LoginButton type="button">로그인</LoginButton>
+              <SignUpFindIdWrap>
+                <button type="button">회원가입</button>
+                <button type="button">아이디/비밀번호 찾기</button>
+              </SignUpFindIdWrap>
+            </LoginModal>
+            <DimdScreen ref={Dimd} onClick={handleLogin}>
+              딤드
+            </DimdScreen>
+          </>
+        )}
       </HomeMain>
-    </div>
+    </HomeBody>
   );
 }
+
+const HomeBody = styled.div`
+  position: relative;
+`;
 
 const HomeMain = styled.div`
   display: flex;
@@ -31,11 +57,20 @@ const HomeMain = styled.div`
 
 const MoveButton = styled.button`
   width: 200px;
-  padding: 20px;
+  height: 100px;
   border: 1px solid #c5e2e8;
   border-radius: 5px;
   font-size: 1.5em;
   background: none;
+  transition: 0.2s;
+  &:active {
+    background-color: #6963e0;
+    color: #fff;
+  }
+  &:hover {
+    background-color: #6963e0;
+    color: #fff;
+  }
 `;
 
 const HomeHead = styled.header`
@@ -52,4 +87,75 @@ const HomeHead = styled.header`
   h1 {
     font-size: 3em;
   }
+`;
+
+const LoginModal = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  position: absolute;
+  z-index: 100;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, 10%);
+  width: 20em;
+  border-radius: 5px;
+  background-color: #fff;
+  padding-bottom: 30px;
+`;
+
+const LoginText = styled.p`
+  width: 100%;
+  text-align: center;
+  padding: 10px 0 10px 0;
+  border-bottom: 1px solid #767676;
+`;
+
+const LoginInputWrap = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  margin: 20px 0 30px 0;
+`;
+
+const IdPassWardInput = styled.input`
+  width: 80%;
+  height: 30px;
+  &::placeholder {
+    padding-left: 10px;
+  }
+`;
+
+const LoginButton = styled.button`
+  width: 80%;
+  height: 30px;
+  margin-bottom: 10px;
+  color: #fff;
+  border-radius: 5px;
+  background-color: #2f80ed;
+`;
+
+const SignUpFindIdWrap = styled.div`
+  display: flex;
+  gap: 10px;
+  button {
+    background: none;
+    color: #767676;
+    font-size: 0.8em;
+  }
+`;
+
+const DimdScreen = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 50;
+  font-size: 0;
+  background: rgba(0, 0, 0, 0.2);
 `;
